159c71d053f5201ba7a922ad6ab7490b3c814ee7
Edith Li <abenoy05@gmail.com>
2016-12-01 13:00:45 -0500

    Update serialComm.ino

--- serialComm.ino
+++ serialComm.ino
@@ -3,6 +3,7 @@ const int buttonPin = 2;     // the number of the pushbutton pin
 // variables will change:
 int buttonState = 0;   
 char text = "a";
+int incomingByte = 0;
 
 void setup()
 {
@@ -16,14 +17,14 @@ void loop()
 
   //receive data
   if (Serial.available() > 0) {
-    char inChar = Serial.read();
-   // inAvailable = true;
+    incomingByte = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
   }
 
   //send data
   if (buttonState == HIGH) {
     Serial.write(text);
-    //buttonState = LOW;
     delay(50);
   }
 }
