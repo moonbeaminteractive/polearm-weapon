8c79aacac6fd6bc56c32e90ecd760a3bbd4d5e28
Edith Li <abenoy05@gmail.com>
2016-12-01 12:38:52 -0500

    Add files via upload

---
+++ serialComm.ino
@@ -0,0 +1,29 @@
+const int buttonPin = 2;     // the number of the pushbutton pin
+ 
+// variables will change:
+int buttonState = 0;   
+char  text[100] = "Text to be transmitted";
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+
+  //receive data
+  if (Serial.available() > 0) {
+    char inChar = Serial.read();
+   // inAvailable = true;
+  }
+
+  //send data
+  if (buttonState == HIGH) {
+    Serial.write(text);
+    //buttonState = LOW;
+    delay(50);
+  }
+}
